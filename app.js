cc5451d754dc771e79ee5c8be94dc00ce632812d
Rohit <moza.rohit@gmail.com>
2017-07-24 17:25:18 -0400

    Vue Lifecycle events

--- app.js
+++ app.js
@@ -1,66 +1,37 @@
-let data = {
-  title: 'DATA TITLE',
-  showParagraph: false
-};
-
-Vue.component('hello', {
-  template: '<h1>Hello!</h1>'
-});
-
-var vm1 = new Vue({
+new Vue({
+  el: '#app',
   data: {
-    title: 'The VueJS Instance',
-    showParagraph: false
+    title: 'The VueJS Instance'
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()');
+  },
+  created: function() {
+    console.log('created()');
+  },
+  beforeMount: function() {
+    console.log('beforeMount()');
+  },
+  mounted: function() {
+    console.log('mounted()');
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate()');
+  },
+  updated: function() {
+    console.log('updated()');
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()')
+  },
+  destroyed: function() {
+    console.log('destroyed()')
   },
   methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
-      this.$refs.myButton.innerText = 'Test';
-    },
-    updateTitle: function(title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
+    destroy: function() {
+      this.$destroy();
     }
   }
-});
 
-//vm1.$refs.heading.innerText = 'Something else';
 
-vm1.$mount('#app1');
-
-setTimeout(function() {
-  vm1.title = 'Changed by Timer';
-},3000);
-
-var vm2 = new Vue({
-  el: '#app2',
-  data: {
-    title: 'VueJS Instance2',
-    showParagraph: false
-  },
-  methods: {
-    onChange: function() {
-      vm1.title = 'Changed!';
-    },
-
-  },
-
-});
-
-var vm3 = new Vue({
-  template: '<h1>Hello!</h1>'
-});
-
-// vm3.$mount();
-// document.getElementById('app3').appendChild(vm3.$el);
-
+})
